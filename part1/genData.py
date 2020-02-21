5c95eaac0a5cf5bdb3e75f59941cdd73ca04aef9
rupark <33532219+rupark@users.noreply.github.com>
2020-02-21 12:10:23 -0500

    Add files via upload

---
+++ part1/genData.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+import csv
+
+with open('employee_file.csv', mode='w') as employee_file:
+        employee_writer = csv.writer(employee_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        for x in range(2000000):
+                employee_writer.writerow(['John', 'Smith', 'Accounting', 'November', 5, 300, 'Graduate', 1.5, 2.5, 49])
+        for x in range(2000000):
+                employee_writer.writerow(['Erica', 'Meyers', 'IT', 'March', 3, 400, 'Non-Graduate', 5.0, 5.0, 22])
+        for x in range(2000000):
+                employee_writer.writerow(['Lisa', 'Jones', 'Marketing', 'June', 10, 50, 'Graduate', 3.0, 4.9, 51])
